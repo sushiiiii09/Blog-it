2b6bea4183cd4e1c00b3f300c5e9e71ecd3a28bd
sushiiiii09 <sshreechoudhury@gmail.com>
2023-09-29 01:00:45 +0530

    fixed: Single Project UI

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,32 +1,35 @@
 import { getProject } from "@/sanity/utils/fetchData";
-import {PortableText} from '@portabletext/react';
+import { PortableText } from '@portabletext/react';
 import Image from "next/image";
- type Props={
-    params:{project:string};
- };
 
- export default async function Project({params}:Props){
-    const slug=params.project;
-    const project=await getProject(slug);
-    return(
-        <div className="">
-        <header className="flex items-center justify-between ">
-          <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl  drop-shadow-sm font-extrabold">
-            {project.title}
-          </h1>
-  
-          <a
-            href={project.url}
-            title="view project"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition duration-500 ease-in-out"
-          >
-            View Project
-          </a>
-        </header>
-        {/* image goes here  */}
-  
+type Props = {
+  params: { project: string };
+};
+
+export default async function Project({ params }: Props) {
+  const slug = params.project;
+  const project = await getProject(slug);
+
+  return (
+    <div className="p-24 border border-red-500">
+      <header className="flex items-center justify-between border border-red-500">
+        <h1 className="text-purple-700 text-5xl drop-shadow-sm font-extrabold">
+          hello
+        </h1>
+
+        <a
+          href={project.url}
+          title="view project"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-purple-500 hover:text-pink-100 transition duration-500 ease-in-out"
+        >
+          View Project
+        </a>
+      </header>
+      <div className="border border-red-500 m-2">
+      {/* Image goes here */}
+      {project.image ? (
         <Image
           src={project.image}
           alt={project.title}
@@ -34,11 +37,14 @@ import Image from "next/image";
           height={1080}
           className="mt-10 border-2 border-gray-700 object-cover rounded-xl"
         />
-  
-        {/* content goes here  */}
-        <div className="text-lg text-gray-700 mt-5 prose">
-          <PortableText value={project.content} />
-        </div>
+      ) : (
+        <div className="p-12 text-xl text-gray-700">No image available 🍃</div>
+      )}
+
+      {/* Content goes here */}
+      <div className="text-lg text-gray-700 mt-5 prose px-12">
+        <PortableText value={project.content} />
       </div>
-    )
- }
+    </div></div>
+  );
+}
